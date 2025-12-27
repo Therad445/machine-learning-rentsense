b25778fb1f5e9a595be509d4261976b452034485
Радмир Исламов <therad445@outlook.com>
2025-12-27 10:28:55 +0300

    test: add metrics unit tests

---
+++ tests/test_metrics.py
@@ -0,0 +1,15 @@
+from rentsense.metrics import mape, evaluate_regression
+
+
+def test_mape_basic():
+    y_true = [100, 200, 300]
+    y_pred = [110, 180, 330]
+    val = mape(y_true, y_pred)
+    assert 0 <= val < 1
+
+
+def test_evaluate_regression_keys():
+    y_true = [1, 2, 3, 4]
+    y_pred = [1, 2, 3, 4]
+    d = evaluate_regression("x", y_true, y_pred)
+    assert set(d.keys()) == {"model", "MAE", "RMSE", "MAPE", "R2"}
